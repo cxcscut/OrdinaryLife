ad28bf48a74be63a46d4bdc61e245f56614322e7
cxcscut <393171302@qq.com>
2017-11-22 19:04:20 +0800

    Animations 2017-11-12

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float pivot;
+
+	public float speed;
+
+	private bool facingRight;
+
+	void Start()
+	{
+		facingRight = true;
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		if (moveHorizontal > 0.0f) {
+			if (!facingRight) {
+				facingRight = true;
+
+			}
+		} 
+		else {
+			if (facingRight) {
+				facingRight = false;
+
+			}
+		}
+		
+		Vector2 move_vector = new Vector2 (moveHorizontal,0.0f);
+
+		pivot += moveHorizontal;
+
+		transform.Translate(move_vector * speed);
+	}
+}
