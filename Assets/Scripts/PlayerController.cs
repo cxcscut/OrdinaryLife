7fffa72a94f5cba67702f3027e52c3ec962a2953
cxcscut <393171302@qq.com>
2017-12-09 14:21:09 +0800

    Scene #1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,42 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour {
 
-	public float pivot;
+	public Animator animator;
 
 	public float speed;
 
 	private bool facingRight;
 
+	void TurnAround()
+	{
+		Vector3 theScale = transform.localScale;
+
+		theScale.x *= -1;
+
+		transform.localScale = theScale;
+	}
+
 	void Start()
 	{
-		facingRight = true;
+		facingRight = false;
 
+		animator = GetComponent<Animator> ();
+
+	}
+
+	void FixedUpdate()
+	{
+		
 	}
 
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
 
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-
+			
 		if (moveHorizontal > 0.0f) {
+			animator.SetInteger ("State",1);
 			if (!facingRight) {
 				facingRight = true;
-
+				TurnAround ();
 			}
 		} 
-		else {
+		else if(moveHorizontal < 0.0f){
+			animator.SetInteger ("State",1);
 			if (facingRight) {
 				facingRight = false;
-
+				TurnAround ();
 			}
 		}
+		else
+			animator.SetInteger ("State",0);
 		
 		Vector2 move_vector = new Vector2 (moveHorizontal,0.0f);
 
-		pivot += moveHorizontal;
-
 		transform.Translate(move_vector * speed);
 	}
+
+
 }
+
